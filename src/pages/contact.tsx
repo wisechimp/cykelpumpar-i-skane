d74929149267c1dc7e04a24b9a402ca6561dff64
shicks1080 <stuart.hicks@gmail.com>
2022-02-25 15:16:50 +0100

    Adding a contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from 'components/Layout'
+
+const Contact = () => (
+  <Layout pageTitle='Contact'>
+    <p>Drop us a line yeah!</p>
+  </Layout>
+)
+
+export default Contact
