f29b36ad24c1f463c60e820ba456020c025d19ee
shicks1080 <stuart.hicks@gmail.com>
2022-02-26 13:58:49 +0100

    Attempting to prevent the gh-pages branch being tested by circleCI

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Testing = () => (
   <div>
-    <p>This is a testing time...</p>
+    <p>This is testing time. Adding a file resulted in a new build as anticipated (hoped!).</p>
+    <p>Let's edit this page and try adding a commit message to the circleCI asking it to nicely skip running tests on the gh-pages branch.</p>
+
   </div>
 )
 
