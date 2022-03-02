e29c2f17d71df7db7e7664868c67d5be892a758b
shicks1080 <stuart.hicks@gmail.com>
2022-03-02 09:45:07 +0100

    Update testing.tsx

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -4,6 +4,7 @@ const Testing = () => (
   <div>
     <p>This is testing time. Adding a file resulted in a new build as anticipated (hoped!).</p>
     <p>Let's edit this page and try adding a commit message to the circleCI asking it to nicely skip running tests on the gh-pages branch again.</p>
+    <p>We should check that the gatsby-node has updated satisfactorily.</p>
   </div>
 )
 
