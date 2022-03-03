c56f2a2ccc2c5e05bda5b52bee8588354272fcdf
shicks1080 <stuart.hicks@gmail.com>
2022-03-03 12:28:34 +0100

    Adding a custom Popup component for when a user clicks on a pump

--- src/components/CyclePumpMap/CyclePumpMap.tsx
+++ src/components/CyclePumpMap/CyclePumpMap.tsx
@@ -1,36 +1,109 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, MouseEvent } from 'react'
 // @ts-ignore
-import { Map } from '!mapbox-gl'
+import { Map, EventData, GeolocateControl, Marker, Popup } from '!mapbox-gl'
 
 import mapboxSettings from '../../settings/mapbox-settings'
+import MapPopup from 'components/MapPopup/MapPopup'
+import ReactDOM from 'react-dom'
+import { stringify } from 'querystring'
 
 const CyclePumpMap = () => {
-  const [longitude, setLongitude] = useState(mapboxSettings.lng)
-  const [latitude, setLatitude] = useState(mapboxSettings.lat)
-  const [zoom, setZoom] = useState(mapboxSettings.zoom)
+  interface CyclePumpData {
+    id: number,
+    properties: {
+      name: string,
+      address: string
+    }
+  }
+
+  interface EventCoordinates {
+    x: number,
+    y: number
+  }
 
   const accessToken = process.env.MAPBOX_ACCESS_TOKEN
 
   const mapNode = useRef(null)
-  let map: Map
+  const cyclePumpPopupRef = useRef(new Popup({ offset: [0, -15] }))
 
   useEffect(() => {
     console.log(accessToken)
     if (!mapNode.current) return
 
-    map = new Map({
+    const map = new Map({
       accessToken,
-      attributionControl: false,
       container: mapNode.current,
       style: "mapbox://styles/wisechimp/ck7bzyusk00uq1iqu32awttdx",
-      center: [longitude, latitude],
-      zoom
+      center: [mapboxSettings.lng, mapboxSettings.lat],
+      zoom: mapboxSettings.zoom
+    })
+      .addControl(
+        new GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true,
+          showUserHeading: true,
+          showUserLocation: true
+        })
+      )
+
+    map.on('mouseenter', 'cykelpumpar-lund', () => {
+      map.getCanvas().style.cursor = 'pointer'
+    })
+
+    map.on('mouseleave', 'cykelpumpar-lund', () => {
+      map.getCanvas().style.cursor = ''
+    })
+
+    map.on("click", (clickData: EventData)  => {
+      console.log("Power click!")
+      const { point, lngLat } = clickData
+
+      const mapMarker = null
+
+      const mapMarkerData = fetchMarkerData(map, mapMarker, point)
+
+      if (mapMarkerData) {
+        console.log("Clicked Pump's id: " + mapMarkerData[0].id)
+        renderPopup(mapMarkerData[0], lngLat).addTo(map)
+      } else {
+        console.log("There's no pump there my friend")
+      }
     })
 
     return () => {
       map.remove()
     }
   }, [])
+
+  const fetchMarkerData = (map: Map, mapMarker: Marker, point: EventData.point) => {
+    if (mapMarker !== null) {
+      mapMarker.remove()
+    }
+
+    const mapFeatures = map.queryRenderedFeatures(point, {
+      layers: ["cykelpumpar-lund"]
+    })
+
+    if (!mapFeatures.length) {
+      return console.log("No Features")
+    }
+    return mapFeatures
+  }
+
+  const renderPopup= (data: CyclePumpData, lngLat: EventCoordinates) => {
+    const { properties } = data
+    const { name, address} = properties
+    const popupNode = document.createElement("div")
+    ReactDOM.render(
+      <MapPopup />,
+      popupNode
+    )
+    return cyclePumpPopupRef.current
+      .setLngLat(lngLat)
+      .setDOMContent(popupNode)
+  }
 
   return (
     <div>
---
+++ src/components/MapPopup/MapPopup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as styles from './mappopup.module.css'
+
+const MapPopup = () => (
+  <div className={styles.mapPopup}>
+    <p>What's popped up here?</p>
+  </div>
+)
+
+export default MapPopup
