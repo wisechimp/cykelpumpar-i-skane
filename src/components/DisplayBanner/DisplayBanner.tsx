a346ed699c456665cfebb2a0932a476d2dd79311
shicks1080 <stuart.hicks@gmail.com>
2022-03-17 08:52:33 +0100

    Renaming the Advert to display banner, plus every reference.
    Because something in the CSS picked up on the banner-ad classname and either blocked it or gave it some other styles from somewhere or other which meant it disappeared!

--- src/components/DisplayBanner/DisplayBanner.tsx
+++ src/components/DisplayBanner/DisplayBanner.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import * as styles from './advert.module.css'
+import * as styles from './displaybanner.module.css'
 
-const Advert = () => (
+const DisplayBanner = () => (
   <div className={styles.banner}>
     <Link to="/contact">Your Message Here!</Link>
   </div>
 )
 
-export default Advert
+export default DisplayBanner
